2aec2c8ade7bcea9466434d087ef3414c44b0532
Ryan Fan <ryanfan@Ryans-MacBook-Pro.local>
2026-03-02 20:28:55 -0500

    update constants.py

--- constants.py
+++ constants.py
@@ -38,4 +38,4 @@ EVENTS_URL_RE = r'https://tcg.ravensburgerplay.com/events/[0-9]+'
 # Get events.json from Github
 GITHUB_OWNER = "gtalorcana"
 GITHUB_REPO  = "gtalorcana.ca"
-UPCOMING_EVENTS_JSON_URL   = f"https://raw.githubusercontent.com/{GITHUB_OWNER}/{GITHUB_REPO}/main/data/events.json"
+UPCOMING_EVENTS_JSON_URL   = f"https://raw.githubusercontent.com/{GITHUB_OWNER}/{GITHUB_REPO}/main/data/upcoming_events.json"
